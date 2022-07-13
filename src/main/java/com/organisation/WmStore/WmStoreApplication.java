2a39b49578af7e42948196bfb669368c487daf57
Maxim <maximgrigorovua@gmail.com>
2022-07-13 12:58:35 +0300

    WmStore

---
+++ src/main/java/com/organisation/WmStore/WmStoreApplication.java
@@ -0,0 +1,13 @@
+package com.organisation.WmStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/organisation/WmStore/WmStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organisation.WmStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
