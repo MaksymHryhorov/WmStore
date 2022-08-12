cc3b983d357c76fd820224daa5d6e6360d1025ce
Maxim <maximgrigorovua@gmail.com>
2022-08-12 14:53:31 +0300

    Connect with FronEnd

--- src/main/java/com/organisation/WmStore/WmStoreApplication.java
+++ src/main/java/com/organisation/WmStore/WmStoreApplication.java
@@ -1,8 +1,14 @@
 package com.organisation.WmStore;
 
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @AutoConfiguration
 @SpringBootApplication
@@ -10,6 +16,23 @@ public class WmStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WmStoreApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration  = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin Accept", "X-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 
 }
